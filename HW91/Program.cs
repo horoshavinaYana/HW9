3f890716d170c736800620aabc1cdbb005e4d46f
Yana <horoshavina_yana@mail.ru>
2023-03-15 21:55:13 +1200

    Задача 1

---
+++ HW91/Program.cs
@@ -0,0 +1,28 @@
+﻿//Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+//N = 5 -> "5, 4, 3, 2, 1"
+//N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+
+int GetIncomingData(string message)
+{
+    Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+void PrintPrevNaturalNumbers(int naturalnumber)
+{
+    if(naturalnumber > 1)
+    {
+        Console.Write($"{naturalnumber},");    
+        naturalnumber--;
+    }
+    else
+    {
+        Console.Write($"{naturalnumber}");    
+        return;
+    };
+    PrintPrevNaturalNumbers(naturalnumber);
+}
+
+int NaturalNumbers = GetIncomingData("Введите натуральное число");
+PrintPrevNaturalNumbers( NaturalNumbers );
