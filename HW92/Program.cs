fe278e0bf999b8f7e2ba1c8bca39b6d8b7609ac3
Yana <horoshavina_yana@mail.ru>
2023-03-15 21:59:29 +1200

    Задача 2

---
+++ HW92/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+//M = 1; N = 15 -> 120
+//M = 4; N = 8. -> 30
+
+int GetIncomingData(string message)
+{
+    Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int GetSumm(int naturalnumber, int high)
+{
+    naturalnumber++;
+    if (naturalnumber == high)
+    {
+        return naturalnumber;
+    };
+    return GetSumm(naturalnumber, high) + naturalnumber;
+}
+
+int Low = GetIncomingData("Введите нижнюю границу натуральным числом");
+int High = GetIncomingData("Введите верхнюю границу натуральным числом");
+Console.WriteLine(GetSumm(Low-1, High));
